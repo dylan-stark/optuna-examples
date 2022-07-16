9ce6b83b391d8d2e0ef8734e095947ac1d999639
Dylan Stark <dylan.stark@gmail.com>
2022-07-16 10:32:53 -0500

    Finishes up logging via Comet SDK.

--- sklearn/sklearn_simple_comet_sdk.py
+++ sklearn/sklearn_simple_comet_sdk.py
@@ -17,25 +17,74 @@ import sklearn.model_selection
 import sklearn.svm
 
 
-def log_trial_to_comet(study: optuna.Study, trial: optuna.trial.FrozenTrial) -> None:
+def log_study_trial(
+    study: optuna.study.Study,
+    trial: optuna.trial.FrozenTrial,
+    metric_name: str = "value"
+) -> None:
     """Logs a single trial for the given study to Comet."""
-    project_name = f"sklearn_simple_comet_sdk_{study.study_name}"
-    experiment = Experiment(project_name=project_name)
+
+    def add_flattened_with_index(source: dict, prefix: str, target: dict) -> dict:
+        """Adds flattened dictionary with indexed set of (name, value) pairs."""
+        for i, key in enumerate(source.keys()):
+            target[f"{prefix}_{i}_name"] = key
+            target[f"{prefix}_{i}_value"] = source[key]
+        return target
+
+    def add_flattened(source: dict, prefix: str, target: dict) -> dict:
+        """Adds flattened dictionary with (name, value) pairs."""
+        for key, value in source.items():
+            target[f"{prefix}_{key}"] = value
+        return target
+
+    experiment = Experiment(project_name=study.study_name)
     experiment.set_name(f"trail_{trial.number}")
+
+    study_info = {
+        "study_best_trial_datetime_complete": study.best_trial.datetime_complete,
+        "study_best_trial_datetime_start": study.best_trial.datetime_start,
+        "study_best_trial_duration": study.best_trial.duration,
+        "study_best_trial_number": study.best_trial.number,
+        "study_best_trial_state": study.best_trial.state,
+        f"study_best_trial_{metric_name}": study.best_trial.value,
+        # TODO: Add support for values from best trial
+        # TODO: Add support for best trials
+        f"study_best_{metric_name}": study.best_value,
+        "study_direction": study.direction,
+        # TODO: Add support for directions
+        "study_pruner": study.pruner,
+        "study_sampler": study.sampler,
+        "study_name": study.study_name,
+    }
+    study_info = add_flattened_with_index(study.best_params, "study_best_params", study_info)
+    study_info = add_flattened_with_index(study.best_trial.distributions, "study_best_trial_distributions", study_info)
+    study_info = add_flattened_with_index(study.best_trial.params, "study_best_trial_params", study_info)
+    study_info = add_flattened(study.best_trial.system_attrs, "study_best_trial_system_attrs", study_info)
+    study_info = add_flattened(study.best_trial.user_attrs, "study_best_trial_user_attrs", study_info)
+    study_info = add_flattened(study.system_attrs, "study_system_attrs", study_info)
+    study_info = add_flattened(study.user_attrs, "study_user_attrs", study_info)
+
+    trial_info = {
+        "trial_datetime_complete": trial.datetime_complete,
+        "trial_datetime_start": trial.datetime_start,
+        "trial_duration": trial.duration,
+        "trial_number": trial.number,
+        "trial_state": trial.state,
+        f"trial_{metric_name}": trial.value,
+    }
+    trial_info = add_flattened_with_index(trial.distributions, "trial_distributions", trial_info)
+    trial_info = add_flattened_with_index(trial.params, "trial_params", trial_info)
+    trial_info = add_flattened(trial.system_attrs, "trial_system_attrs", trial_info)
+    trial_info = add_flattened(trial.user_attrs, "trial_user_attrs", trial_info)
 
     experiment.log_parameters(trial.params)
     experiment.log_metrics(
         dic={
-            "value": trial.value,
+            metric_name: trial.value,
         },
         step=0)
-    experiment.log_others({
-        "trial_number": trial.number,
-        "trial_datetime_start": trial.datetime_start,
-        "trial_system_attrs": trial.system_attrs,
-        "trial_user_attrs": trial.user_attrs,
-        "study_direction": study.direction,
-    })
+    experiment.log_others(study_info)
+    experiment.log_others(trial_info)
     experiment.end()
 
 
@@ -63,8 +112,8 @@ def objective(trial):
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective, n_trials=10)
     print(study.best_trial)
 
     for trial in study.get_trials(deepcopy=False):
-        log_trial_to_comet(study, trial)
+        log_study_trial(study, trial, metric_name="accuracy")
